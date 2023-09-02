684efead6d0e170524c54efd5e86a8330ce63123
Adina Sarsenbayeva <125823447+breezevna@users.noreply.github.com>
2023-09-02 18:35:32 +0200

    Add files via upload

---
+++ tictactoe/tictactoe/GameLogic.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace tictactoe
+{
+    public class GameLogic
+    {
+        public string CurrentPlayer { get; private set; }
+        public string TextBox11 { get; set; }
+        public string TextBox12 { get; set; }
+        public string TextBox13 { get; set; }
+
+        public string TextBox21 { get; set; }
+        public string TextBox22 { get; set; }
+        public string TextBox23 { get; set; }
+
+        public string TextBox31 { get; set; }
+        public string TextBox32 { get; set; }
+        public string TextBox33 { get; set; }
+
+        public GameLogic(string textBox11, string textBox12, string textBox13,
+            string textBox21, string textBox22, string textBox23,
+            string textBox31, string textBox32, string textBox33)
+        {
+            Random random = new Random();
+            int move = random.Next(1, 3);
+            if(move == 1) CurrentPlayer = "X";
+            else CurrentPlayer = "O";
+            TextBox11 = textBox11;
+            TextBox12 = textBox12;
+            TextBox13 = textBox13;
+            TextBox21 = textBox21;
+            TextBox22 = textBox22;
+            TextBox23 = textBox23;
+            TextBox31 = textBox31;
+            TextBox32 = textBox32;
+            TextBox33 = textBox33;
+        }
+
+        public void UpdateValues(string textBox11, string textBox12, string textBox13,
+            string textBox21, string textBox22, string textBox23,
+            string textBox31, string textBox32, string textBox33)
+        {
+            TextBox11 = textBox11;
+            TextBox12 = textBox12;
+            TextBox13 = textBox13;
+            TextBox21 = textBox21;
+            TextBox22 = textBox22;
+            TextBox23 = textBox23;
+            TextBox31 = textBox31;
+            TextBox32 = textBox32;
+            TextBox33 = textBox33;
+        }
+        public string NextPlayer()
+        {
+            CurrentPlayer = (CurrentPlayer == "X") ? "O" : "X";
+            return CurrentPlayer;
+        }
+
+        public string ShowWinners(string winners)
+        {
+            string player;
+            if (winners == "X") player = "X";
+            if (winners == "O") player = "O";
+            else player = "Draw";
+            return player;
+        }
+
+        public string CheckWinners()
+        {
+            string player = " ";
+
+            string[,] board = new string[,]
+            {
+                {TextBox11,TextBox12, TextBox13},
+                {TextBox21,TextBox22, TextBox23},
+                {TextBox31,TextBox32, TextBox33}
+            };
+
+
+                for (int row = 0; row < 3; row++)
+                {
+                    if (board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2] && !string.IsNullOrEmpty(board[row, 0]))
+                    {
+                        if (board[row, 0] == "X") player = "X";
+                        else player = "O";
+                    }
+                }
+
+                for (int column = 0; column < 3; column++)
+                {
+                    if (board[0, column] == board[1, column] && board[1, column] == board[2, column] && !string.IsNullOrEmpty(board[0, column]))
+                    {
+                        if (board[0, column] == "X") player = "X";
+                        else player = "O";
+                    }
+                }
+
+                if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && !string.IsNullOrEmpty(board[0, 0]))
+                {
+                    if (board[0, 0] == "X") player = "X";
+                    else player = "O";
+                }
+
+                if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && !string.IsNullOrEmpty(board[0, 2]))
+                {
+                    if (board[0, 2] == "X") player = "X";
+                    else player = "O";
+                }
+            
+            return player;
+        }
+
+
+    }
+}
---
+++ tictactoe/tictactoe/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tictactoe
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GameLogic gameLogic;
+
+        private bool IsGameEnded;
+
+        private string winner;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gameLogic = new GameLogic(TextBlock11.Text, TextBlock12.Text, TextBlock13.Text,
+                                      TextBlock21.Text, TextBlock22.Text, TextBlock23.Text,
+                                      TextBlock31.Text, TextBlock32.Text, TextBlock33.Text);
+        }
+
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            IsGameEnded = false;
+            TextBlock textBlock = (TextBlock)sender;
+            Border border = (Border)textBlock.Parent;
+
+            if (gameLogic.NextPlayer() == "X")
+            {
+                border.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#8EF13C"));
+                textBlock.Text = "X";
+                textBlock.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#8EF13C"));
+                Player1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#8EF13C"));
+                Player2.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CCCCCC"));
+            }
+            else
+            {
+                border.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF2400"));
+                textBlock.Text = "O";
+                textBlock.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF2400"));
+                Player2.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF2400"));
+                Player1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CCCCCC"));
+            }
+            gameLogic.UpdateValues(TextBlock11.Text, TextBlock12.Text, TextBlock13.Text,
+                                      TextBlock21.Text, TextBlock22.Text, TextBlock23.Text,
+                                      TextBlock31.Text, TextBlock32.Text, TextBlock33.Text);
+
+            winner = gameLogic.CheckWinners();
+
+            if (winner == "X") 
+            { 
+                Win.Text = "Won: Player 1";
+                IsGameEnded = true;
+            }
+            if (winner == "O")
+            {
+                Win.Text = "Won: Player 2";
+                IsGameEnded = true;
+            }
+
+            int count = 0;
+            for(int i = 1; i <= 3; i++)
+            {
+                for(int j = 1; j <= 3; j++)
+                {
+                    string textBlockName = $"TextBlock{i}{j}";
+                    TextBlock textNameFinder = (TextBlock)grid.FindName(textBlockName);
+                     if(textNameFinder.Text != "")
+                     {
+                        count++;
+                     }
+                }
+            }
+
+            if(count == 9 && Win.Text  == "Won:")
+            {
+                Win.Text = "Draw!";
+            }
+            if(IsGameEnded == true)
+            {
+                TextBlock11.IsEnabled = false;
+                TextBlock12.IsEnabled = false;
+                TextBlock13.IsEnabled = false;
+                TextBlock21.IsEnabled = false;
+                TextBlock22.IsEnabled = false;
+                TextBlock23.IsEnabled = false;
+                TextBlock31.IsEnabled = false;
+                TextBlock32.IsEnabled = false;
+                TextBlock33.IsEnabled = false;
+            }
+        }
+
+        private void btnRestar_Click(object sender, RoutedEventArgs e)
+        {
+            for(int i = 1; i <= 3; i++)
+            {
+                for(int j = 1; j <= 3; j++)
+                {
+                    string textBlockName = $"TextBlock{i}{j}";
+                    string borderName = $"Border{i}{j}";
+
+                    Border border = (Border)grid.FindName(borderName);
+                    TextBlock testBlock = (TextBlock)grid.FindName(textBlockName);
+                    testBlock.IsEnabled = true;
+                    testBlock.Text = "";
+                    border.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CCCCCC"));
+                }
+            }
+            Win.Text = "Won:";
+            Player1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CCCCCC"));
+            Player2.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CCCCCC"));
+
+        }
+    }
+}
